67acc70375ef115139bc7f3f40cbb3946770c90d
rafapm1999 <rportillom99.code@gmail.com>
2024-09-23 21:26:16 +0200

    Modificate routes in apiRoutes.js

--- backend/Routes/apiRoutes.js
+++ backend/Routes/apiRoutes.js
@@ -5,12 +5,13 @@ const router = express.Router();
 //Importamos apiController para generar rutas a los endpoints
 const apiController = require("../Controller/apiController");
 
-router.post("/games", apiController.games);
+router.post("/games/:id", apiController.games);
 router.post("/games/:id/join", apiController.idJoin);
 router.post("/games/:id/draw", apiController.idDraw);
 router.post("/games/:id/play", apiController.idPlay);
 router.get("/games/:id", apiController.id);
 router.get("/games/deck", apiController.deck);
+router.get("games/players", apiController.players)
 
 module.exports = router;
 
