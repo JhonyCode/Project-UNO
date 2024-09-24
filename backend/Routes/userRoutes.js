2a9f37105e5e62eb7ca6d85c040ab333f5910e9c
rafapm1999 <rportillom99.code@gmail.com>
2024-09-24 19:08:43 +0200

    Commit rebase

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,11 +1,17 @@
-//Importamos express
 const express = require('express');
-//Importamos express para poder usar router para generar las rutas
 const router = express.Router();
-//Importamos apiController para generar rutas a los endpoints
-const userController = require("../Controller/userController");
+const User = require('../models/User'); 
 
-router.post("/create/create-user", userController.postCreateUser);
-
+router.post('/', async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = new User({ username, email, password }); 
+    await newUser.save();
+    res.status(201).json(newUser); // Devolvemos el usuario creado
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Error creating user' });
+  }
+});
 
 module.exports = router;
