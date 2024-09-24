157ed3ec02057df543f09f15332b51e2dbd38a4f
rafapm1999 <rportillom99.code@gmail.com>
2024-09-24 18:58:03 +0200

    Write some lines in userRoutes.js

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,17 +1,11 @@
+//Importamos express
 const express = require('express');
+//Importamos express para poder usar router para generar las rutas
 const router = express.Router();
-const User = require('../models/User'); 
+//Importamos apiController para generar rutas a los endpoints
+const userController = require("../Controller/userController");
 
-router.post('/', async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const newUser = new User({ username, email, password }); 
-    await newUser.save();
-    res.status(201).json(newUser); // Devolvemos el usuario creado
-  } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Error creating user' });
-  }
-});
+router.post("/create/create-user", userController.postCreateUser);
+
 
 module.exports = router;
